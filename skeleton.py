cf4903941f7d47aca72ae958a9cc776165ba7098
tkillip7 <tkp3170@gmail.com>
2022-04-12 18:12:42 -0400

    skeleton

---
+++ skeleton.py
@@ -0,0 +1,69 @@
+
+    
+class Player;
+    
+    def __init__():
+        temp = "hi"
+        
+    def turn():
+        temp = "hi"
+        
+class Runningback(Player):
+    temp = "hi"
+
+class Opponent(Player):
+    temp = "hi"
+    
+        
+    
+    
+    
+    
+    
+class Game: 
+    
+    def __init__():
+        temp = "hi"
+    
+        
+    def forward_movement():
+        temp = "hi"
+        
+        
+    def opp():
+        temp = "hi"
+    
+    def get_score():
+        temp = "hi"
+        
+    def reset():
+        temp = "hi"
+    
+    
+        """
+    
+    
+    score:  7                  
+    
+        |    |    |    |    |      
+        |    |    | o  |    |      
+        |    |    |    |    |      
+        |    |    | o  |    |    * 
+        |    |    |    |    |      
+        |    |    | o  |    |      
+        |    |    |    |    |      
+    
+    (player and opponents only move up and down as field scrolls left)
+    (every time player moves up or down, opponents also move up or down randomly)
+    (then the field shuffles 1 block right)
+    
+        |    |    |     
+        | o  |    |     
+        |    |    |     
+        | o  |    |   * 
+        |    |    |     
+        | o  |    |     
+        |    |    |     
+    
+    """
+    
