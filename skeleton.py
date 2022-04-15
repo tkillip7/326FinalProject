dc3c253e6109dc00ba6df4e886cb1ae8e1544eef
tkillip7 <tkp3170@gmail.com>
2022-04-15 18:08:58 -0400

    changed skeleton

--- skeleton.py
+++ skeleton.py
@@ -1,69 +1,15 @@
+class Pokemon:
+    """A Pokemon has a type, health, a set of moves, attack stat, defense stat"""
 
-    
-class Player;
-    
-    def __init__():
-        temp = "hi"
-        
-    def turn():
-        temp = "hi"
-        
-class Runningback(Player):
-    temp = "hi"
 
-class Opponent(Player):
-    temp = "hi"
-    
-        
-    
-    
-    
-    
-    
-class Game: 
-    
-    def __init__():
-        temp = "hi"
-    
-        
-    def forward_movement():
-        temp = "hi"
-        
-        
-    def opp():
-        temp = "hi"
-    
-    def get_score():
-        temp = "hi"
-        
-    def reset():
-        temp = "hi"
-    
-    
-        """
-    
-    
-    score:  7                  
-    
-        |    |    |    |    |      
-        |    |    | o  |    |      
-        |    |    |    |    |      
-        |    |    | o  |    |    * 
-        |    |    |    |    |      
-        |    |    | o  |    |      
-        |    |    |    |    |      
-    
-    (player and opponents only move up and down as field scrolls left)
-    (every time player moves up or down, opponents also move up or down randomly)
-    (then the field shuffles 1 block right)
-    
-        |    |    |     
-        | o  |    |     
-        |    |    |     
-        | o  |    |   * 
-        |    |    |     
-        | o  |    |     
-        |    |    |     
-    
+
+
+class Battle:
+    """How the battle will work. Player chooses a pokemon and inputs values
+    in terminal to fight the computer's pokemon.
     """
+
+
+
+
     
