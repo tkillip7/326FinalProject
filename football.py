bbf62b91288355dae3a5f9d409d3445985ee370a
Daniel H Gonzalez <raptor3500@hotmail.com>
2022-04-14 21:30:19 -0400

    formating

--- football.py
+++ football.py
@@ -1,35 +1,57 @@
-
-
-
-
+ 
 class Pokemon:
-    """_summary_
+   """ Initializes the 6 pokemon and their attributes.
+   """
+   def __init__(self,player,opponent):
+       temp = "hi"
+      
+   def player(self):
+       temp = "hi"
+     
+  
+class Fight:
+    """Initializes the battle between the pokemon. Includes move selection and damage            
+     calculations
+    
     """
-    def __init__():
+    def __init__(self):
+       temp = "hi"
+  
+      
+    def move_select(self):
+       temp = "hi"
+      
+      
+    def damage_calc(self,move):
+        """Checks the damage the selected move would do to the opposing pokemon.
+	        Accounts for attack Stat, attack power of the move, defense stat, typing, 
+   	    Args:
+      	    arglist (list of str): arguments from the command line.
+ 	    Returns:
+       	    namespace: the parsed arguments, as a namespace.
+        """
         temp = "hi"
+  
+    def accuracy(self,move):
+        """Performs a check to see if the selected move will connect. Random Number Generator for choosing accuracy. 
         
-    def turn():
+        """
         temp = "hi"
-       
-    
-class Fight: 
-    
-    def __init__():
-        temp = "hi"
-    
-        
-    def forward_movement():
-        temp = "hi"
-        
-        
-    def opp():
-        temp = "hi"
-    
-    def get_score():
-        temp = "hi"
-        
+      
     def reset():
         temp = "hi"
-    
 
-    
+if __name__ == "__main__":
+   args = parse_args(sys.argv[1:])
+   main(args.file)
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+
